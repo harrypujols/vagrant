cc4e3439a4fb3d65842ceb92646b317300267805
Harry Pujols <harrypujols@me.com>
2016-10-05 00:14:34 -0400

    Init

---
+++ bootstrap.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='root'
+PROJECTFOLDER='public'
+
+# create project folder
+sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install apache
+sudo apt-get install -y apache2
+
+if ! [ -L /var/www ]; then
+  rm -rf /var/www/html
+  ln -fs /vagrant/$PROJECTFOLDER /var/www/html
+fi
+
+# install php5
+sudo apt-get install -y php5 libapache2
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install -y php5-mysql
+
+# create a database
+mysql -uroot -e "create database ${PROJECTFOLDER};"
+
+# install phpmyadmin and give password(s) to installer
+# for simplicity I'm using the same password for mysql and phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+sudo php5enmod mcrypt
+
+# setup mysql user
+MSQL=$(cat <<EOF
+[client]
+user=root
+password=root
+host=localhost
+EOF
+)
+echo "${MSQL}" > /home/vagrant/.my.cnf
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html"
+    <Directory "/var/www/html">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/public.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+sudo apt-get -y install git
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Web page</title>
+  </head>
+  <body>
+    <h1>Hello <?php echo "World"; ?></h1>
+    <a href="http://166.166.66.60/phpmyadmin/">Go to PHPMyAdmin</a>
+  </body>
+</html>
