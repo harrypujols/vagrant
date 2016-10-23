8b5bdb267c17086a4bfcc0c631a4d8d2e81e3139
Harry Pujols <harrypujols@me.com>
2016-10-22 23:19:05 -0400

    more robust html first page

--- public/index.php
+++ public/index.php
@@ -3,9 +3,18 @@
   <head>
     <meta charset="utf-8">
     <title>Web page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
   <body>
-    <h1>Hello <?php echo "World"; ?></h1>
-    <a href="http://166.166.66.60/phpmyadmin/">Go to PHPMyAdmin</a>
+    <div class="container">
+      <h1>Hello <?php echo "World"; ?></h1>
+      <p class="lead">Go to PHPMyAdmin</p>
+      <p><a class="btn btn-primary" href="http://166.166.66.60/phpmyadmin/">PHPMyAdmin</a></p>
+      <p>
+        user: <b>root</b><br>
+        password: <b>root</b>
+      </p>
+    </div>
   </body>
 </html>
