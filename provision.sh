94c771ac60d33ca8b906ab443ba78f9269f11bfa
Harry Pujols <harrypujols@me.com>
2017-08-02 16:03:08 -0400

    Adding webmin

--- provision.sh
+++ provision.sh
@@ -6,32 +6,32 @@ PROJECT=$1
 IP=$2
 
 # update / upgrade
-sudo apt-get update
+apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
 # install apache
-sudo apt-get install -y apache2
+apt-get install -y apache2
 
 # install php latest
-sudo apt-get install -y php libapache2
+apt-get install -y php libapache2
 
 # install composer
-sudo apt-get install -y zip unzip composer
+apt-get install -y zip unzip composer
 
 # install mysql and give password to installer
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server php-mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+apt-get install -y mysql-server php-mysql
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
-sudo phpenmod mcrypt
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+apt-get install -y phpmyadmin
+phpenmod mcrypt
 
 # setup mysql user
 MY=$(cat <<EOF
@@ -41,25 +41,34 @@ password=root
 host=localhost
 EOF
 )
+
 echo "$MY" > /home/vagrant/.my.cnf
 
 # create a database
 mysql --user=$PASSWORD --password=$PASSWORD -e "create database $PROJECT;"
 
 # enable mod_rewrite
-sudo a2enmod rewrite
-sudo a2enmod headers
-sudo a2enmod expires
-sudo a2enmod include
+a2enmod rewrite
+a2enmod headers
+a2enmod expires
+a2enmod include
 
 # restart apache
 service apache2 restart
 
 # symlink site's folder
-sudo ln -s /var/www/html /home/vagrant/$PROJECT
+ln -s /var/www/html /home/vagrant/$PROJECT
 
 # install git
-sudo apt-get -y install git
+apt-get install -y git
+
+# install webmin
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+apt-get update
+apt-get install -y webmin
 
 # all done
+echo "Local webmin running on https://$IP:10000"
 printf "\033[0;36m$PROJECT site running on \033[0;35mhttp://$IP\033[0m"
